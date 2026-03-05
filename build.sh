66fc46408d7b48e5e8893de1c69b02f7090f2bdb
Jesse Sher <jebba272727@gmail.com>
2026-03-06 08:04:54 +1100

    changed build.sh to work with mac well

--- build.sh
+++ build.sh
@@ -13,4 +13,15 @@ CODE_FILES=$(find src/ -name "*.cpp")
 
 # Build command
 $CC $CODE_FILES $LIB_DIR/libraylib.mac.a -o app $CFLAGS -I $INCLUDE_DIR -L $LIB_DIR -lraylib.mac -framework OpenGL -framework Cocoa -framework IOKit -framework CoreVideo
-./app
+
+# This copies all the needed things for the app and puts it in
+# /Users/sher.jesse/temp_app
+# This allows for it to be ran without the OneDrive problem
+DIR_NAME=$(basename "$(pwd)")
+mkdir /Users/sher.jesse/temp_app/$DIR_NAME
+rsync -av \
+  --exclude 'src' \
+  --exclude 'include' \
+  --exclude 'lib' \
+  . "/Users/sher.jesse/temp_app/$DIR_NAME/app"
+/Users/sher.jesse/temp_app/$DIR_NAME/app
